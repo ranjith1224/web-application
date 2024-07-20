72d62ebcff5d41969edba21dbad56ebbc17a7819
ranjith <ranjith.d2221@gmail.com>
2024-07-20 16:13:19 +0530

    new

--- web-1a.tf
+++ web-1a.tf
@@ -1,10 +1,10 @@
 ## Ec2 instance and web server 
 resource "aws_instance" "web-1a" { 
 
-ami = "ami-03bb6d83c60fc5f7c"
-instance_type = "t2.small"
+ami = "ami-0ad21ae1d0696ad58"
+instance_type = "t2.micro"
 subnet_id = aws_subnet.web-pub-1a.id
-key_name = "Mumbairegion"
+key_name = ""
 security_groups = [aws_security_group.web_server_sg_tf.id]
 count = 1
 
